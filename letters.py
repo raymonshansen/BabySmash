b485682b3dd11ce2b19fdfb9b29d669a557f5517
Raymon Skjørten Hansen <raymon.s.hansen@gmail.com>
2019-11-21 13:38:11 +0100

    Letters is now run by the App class

--- letters.py
+++ letters.py
@@ -6,6 +6,7 @@ from random import randint
 import constants as cons
 from utils import rand_color, rand_screen_pos
 from particles import ParticleGenerator
+from character import Character
 
 
 def get_character(event_key):
@@ -23,61 +24,18 @@ def get_character(event_key):
         return ' '
 
 
-class Character():
-    def __init__(self, char, pos, size):
-        self.char = char
-        self.font = pg.font.SysFont('ubuntumono', size, 1)
-        self.color = rand_color()
-        self.origsurf = self.font.render(self.char, True, rand_color())
-        self.textsurf = self.origsurf.copy()
-        self.alphasurf = pg.Surface(self.textsurf.get_size(), pg.SRCALPHA)
-        self.alpha = 255
-        self.fading = False
-        self.pos = pos
-
-    def get_rect(self):
-        return pg.Rect(self.pos, self.textsurf.get_size())
-
-    def __str__(self):
-        return self.char
-
-    def draw(self, screen):
-        if self.char == ' ':
-            return
-        if self.fading:
-            self.textsurf = self.origsurf.copy()
-            self.alphasurf.fill((255, 255, 255, self.alpha))
-            self.textsurf.blit(self.alphasurf, (0, 0), special_flags=pg.BLEND_RGBA_MULT)
-            self.alpha = max(self.alpha - 10, 0)
-        screen.blit(self.textsurf, self.pos)
-
-    def fade_out(self):
-        self.fading = True
-
-
-class Game():
-    def __init__(self):
+class Letters():
+    def __init__(self, screen, switch_state_func):
         pg.init()
         pg.font.init()
-        self.width = cons.WINDOW_WIDTH
-        self.height = cons.WINDOW_HEIGHT
-        self.screen = pg.display.set_mode((self.width, self.height))
+        self.switch_state = switch_state_func
+        self.screen = screen
         self.clock = pg.time.Clock()
         self.running = True
         self.char_buffer = [Character(' ', (0, 0), 1)] * 5
 
         # Is this a separate game?? :)
         self.part_gen = ParticleGenerator(0, 0, 1, self.screen, rand_color())
-
-    def run(self):
-        self.screen.fill(pg.color.Color(cons.BACKGROUND_COLOR))
-        pg.display.update()
-        while self.running:
-            self.update()
-            self.draw()
-            self.clock.tick(cons.FPS)
-        pg.quit()
-        sys.exit(0)
 
     def non_overlapping_character(self, char):
         """Return a character_object that does not overlap
@@ -109,8 +67,8 @@ class Game():
             if event.type == MOUSEBUTTONUP:
                 self.part_gen.active = False
                 self.part_gen.clear()
-        self.running = self.check_quit()
         self.part_gen.update()
+        self.check_quit()
 
     def draw(self):
         self.screen.fill(pg.color.Color(cons.BACKGROUND_COLOR))
@@ -121,7 +79,7 @@ class Game():
 
     def buffer_to_string(self):
         return ''.join(str(char) for char in self.char_buffer)
-    
+
     def buffer_to_rects(self):
         return [char.get_rect() for char in self.char_buffer]
 
@@ -130,9 +88,5 @@ class Game():
         self.char_buffer.pop(0)
 
     def check_quit(self):
-        return self.buffer_to_string()[-4:] != 'QUIT'
-
-
-if __name__ == "__main__":
-    game = Game()
-    game.run()
+        if self.buffer_to_string()[-4:] == 'QUIT':
+            self.switch_state('MAINMENU')
