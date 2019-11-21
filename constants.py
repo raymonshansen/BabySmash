2fb1aafd4474ca7377e419ed73097dc643b771fb
Raymon Skjørten Hansen <raymon.s.hansen@gmail.com>
2019-11-21 13:37:13 +0100

    Add more constants, menustuff mostly

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
+SCREEN_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT
 FPS = 30
 THECOLORS = ['blue',
              'brown',
@@ -16,9 +17,24 @@ THECOLORS = ['blue',
              'red',
              'yellow',
              ]
+# MENU STUFF
+MENU_LINE_PAD = WINDOW_WIDTH // 100
+MENU_LINE_LENGTH = WINDOW_WIDTH // 3
+MENU_TEXT_COLOR = 'lightblue'
+MENU_SELECTED_COLOR = 'blue'
+MENU_TEXT_SIZE = 110
+MENU_SELECTED_TEXT_SIZE = 130
+
+
 ALPHABET = 'abcdefghijklmnopqrstuvwxyzæøå'
 BACKGROUND_COLOR = 'gray98'
 PARTICLE_MIN = 30
 PARTICLE_MAX = 70
 MAX_LETTER_SIZE = 800
 MIN_LETTER_SIZE = 64
+
+MENU_ITEM_TEXTS = [
+    ("Letters", "Hit a letter-key to display it on screen. Type 'quit' to return to main menu.", "LETTERS"),
+    ("EXIT", "Exit the program.", "QUIT")
+    ]
+APP_NAME = "BabySmash"
